67590988df668c8dadb2fa052824c441a4d616ac
ishowta <ishowta@gmail.com>
2020-09-17 15:56:43 +0900

    Change server language from Python to Nim

---
+++ server/server.nim
@@ -0,0 +1,44 @@
+import base64, strformat
+import jester, redis
+
+proc connectRedis():Future[AsyncRedis]{.async.} = return await openAsync("localhost", 3002.Port)
+
+proc main() {.async.} =
+    let r = await connectRedis()
+
+    router todoRouter:
+        get "/@room_name":
+            let room_name = @"room_name"
+            request.sendHeaders(Http200, @{"Content-Type": "text/event-stream","Access-Control-Allow-Origin": "*","X-Accel-Buffering":"no"})
+            
+            # Get Todo
+            let todo = await r.get(room_name)
+            if todo != redisNil:
+                let raw_todo = decode todo
+                request.send(fmt"data: {raw_todo}" & "\n\n")
+
+            # Subscribe Todo
+            let sub = await connectRedis()
+            await sub.subscribe(room_name)
+            while true:
+                let todo = (await sub.nextMessage()).message
+                let raw_todo = decode todo
+                request.send(fmt"data: {raw_todo}" & "\n\n")
+
+        post "/@room_name":
+            let raw_todo = request.body()
+            let todo = encode raw_todo
+            discard r.setk(@"room_name", todo)
+            discard r.publish(@"room_name", todo)
+            resp Http200, {"Access-Control-Allow-Origin": "*"}, "ok"
+
+    var jester = initJester(
+        matcher=todoRouter,
+        settings=newSettings(
+            port = 3001.Port,
+            bindAddr = "0.0.0.0"
+        )
+    )
+    jester.serve()
+
+waitFor main()
---
+++ server/server.nimble
@@ -0,0 +1,15 @@
+# Package
+
+version       = "0.1.0"
+author        = "ishowta"
+description   = "DAG-ToDo Server"
+license       = "MIT"
+bin           = @["server"]
+
+
+
+# Dependencies
+
+requires "nim >= 1.2.6"
+requires "redis >= 0.3.0"
+requires "jester >= 0.4.3"
